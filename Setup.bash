4629e789046e57c786104716b5afaa136df3a514
TheDoctor-JI <yifanchangle@163.com>
2023-07-17 16:28:55 +0800

    Added vad launchng

--- Setup.bash
+++ Setup.bash
@@ -1,3 +1,13 @@
+#This script is supposed to run on Grace nuc
+
+#Config master machine ip
 source ./IP_Setup_Master.bash
-xterm -hold -e "sleep 30;source ./IP_Setup_Master.bash;python3 ./conv_ai_performances.py --topic='/grace_performance'"&
+
+#Start custom vad process
+xterm -hold -e "python3 grace_vad.py"
+
+#Performance connector is now legacy
+#xterm -hold -e "sleep 30;source ./IP_Setup_Master.bash;python3 ./conv_ai_performances.py --topic='/grace_performance'"&
+
+#Start HRSDK
 hrsdk start --storage ~/workspace/hrsdk_configs/ --head grace9 --body bodyNC
